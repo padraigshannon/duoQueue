58dd78ac8da9fbc4e32ca93336675066b43d42d1
proche13 <23379707@studentmail.ul.ie>
2026-03-13 18:50:17 +0000

    whatever

--- login.php
+++ login.php
@@ -18,7 +18,18 @@
         </nav>
 
         <div class="content">
-            
+
+            <div class="login-box">
+                <h2>Login</h2>
+
+                <form>
+                    <input type="email" placeholder="Email" required>
+                    <input type="password" placeholder="Password" required>
+
+                    <button type="submit">Login</button>
+                </form>
+            </div>
+
         </div>
 
     </div>
