e0ed95cf1d3761e85139b59cacc44d207cbe93f0
Oran Counihan <23366044@studentmail.ul.ie>
2026-03-20 16:31:05 +0000

    Changed landing to index

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>DuoQueue</title>
+    <link rel="stylesheet" href="main.css">
+    <link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap" rel="stylesheet">
+</head>
+
+<body>
+
+    <div class="screen">
+
+        <div class="content">
+            <h1>Welcome to DuoQueue</h1>
+            <img src="assets/logoImage.png" alt="Logo Image" class="logo">
+            <h3>Press Start to find love...</h3>
+        </div>
+
+    </div>
+
+</body>
+</html>
